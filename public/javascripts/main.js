8c8953c0f8c3927c7a117e54773baaada569ec6c
Tokyodays <kikuchi@gmail.com>
2017-07-12 09:02:21 +0900

    wip

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,5 @@
 $(function() {
-  var socket = io.connect();
+  var socket = io.connect('http://localhost:3000/stream/');
   $('form').submit(function() {
     socket.emit('msg', $('input').val());
     $('input').val('');
